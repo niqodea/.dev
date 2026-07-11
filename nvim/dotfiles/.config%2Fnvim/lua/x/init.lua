26658e79d71280e7c9a834099f63e6795a27d3d2
Nicola De Angeli <112023843+niqodea@users.noreply.github.com>
2026-07-11 14:08:50 +0200

    refactor: use shell script to create vx autoload file

--- nvim/dotfiles/.config%2Fnvim/lua/x/init.lua
+++ nvim/dotfiles/.config%2Fnvim/lua/x/init.lua
@@ -33,33 +33,17 @@ end
 
 -- Startup logic for automatic loading of modules
 local atdir = utils.get_atdir()
-local auto_path = atdir .. '/auto.nvim-x'
+local auto_path = atdir .. '/vx-autoload'
 
 if vim.fn.filereadable(auto_path) == 1 then
     local auto_file = io.open(auto_path, 'r')
-    for line in auto_file:lines() do
-        if line:match('^%s*#') or line:match('^%s*$') then
-            goto continue
-        end
-        local module = line:match('^%s*(%S+)')
-        local module_path = 'x.modules.' .. module
+    for module in auto_file:lines() do
+        local module_path = modules_path .. '.' .. module
         require(module_path)
         ::continue::
     end
     auto_file:close()
 end
-
-vim.api.nvim_create_user_command('XAuto', function()
-    vim.cmd('edit ' .. auto_path)
-    if vim.fn.filereadable(auto_path) == 1 then
-        return
-    end
-    local lines = {}
-    for _, module in ipairs(modules) do
-        table.insert(lines, '# ' .. module)
-    end
-    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
-end, {})
 
 vim.cmd('colorscheme retrobox')
 
--- nvim/exported-dotfiles/nvim.zsh
+++ nvim/exported-dotfiles/nvim.zsh
@@ -2,4 +2,3 @@ export EDITOR='nvim'
 
 alias v='nvim'
 alias vx='v -c X'
-alias vxa='vx -c XAuto'
